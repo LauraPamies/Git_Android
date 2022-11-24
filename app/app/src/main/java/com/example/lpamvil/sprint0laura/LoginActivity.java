4a1a699c94b14d9c72cecf672257928380fb7cd0
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2022-11-24 23:32:09 +0100

    link web

--- app/app/src/main/java/com/example/lpamvil/sprint0laura/LoginActivity.java
+++ app/app/src/main/java/com/example/lpamvil/sprint0laura/LoginActivity.java
@@ -39,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private TextView _btn_link; //link
-    String _url = "https://es.wikipedia.org/wiki/Aliasing";//link
+    String _url = "http://localhost:4000/#/recover_pass";//link
 
 
     @Override
